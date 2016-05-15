f928ff4f11ed4776c31d0c8d5c14f111b5c7ff8f
dzemila sehovic <dsehovic1@etf.unsa.ba>
2016-05-15 14:20:29 +0200

    komentari
    komentari cekamo metode ostale

--- Implementacija/src/test/java/ba/unsa/etf/si/tim11/bll/KomentarRepositoryTest.java
+++ Implementacija/src/test/java/ba/unsa/etf/si/tim11/bll/KomentarRepositoryTest.java
@@ -21,7 +21,7 @@ public class KomentarRepositoryTest {
 
 	@Test
 	public void testDodajKomentar() {
-		KorisnikDbModel korModel = new KorisnikDbModel();
+	/*	KorisnikDbModel korModel = new KorisnikDbModel();
 		KorisnikPozicijaDbModel pozicija = new KorisnikPozicijaDbModel();
 		KorisnikTipDbModel korTip = new KorisnikTipDbModel();
 		korTip.setAktivan(true);
@@ -41,10 +41,10 @@ public class KomentarRepositoryTest {
 		korModel.setPassword("pass");
 		korModel.setPrezime("neko prezime");
 		korModel.setUsername("user");
-		long idKor = DbDMSContext.getInstance().getKorisnici().sacuvaj(korModel);
+		long idKor = DbDMSContext.getInstance().getKorisnici().sacuvaj(korModel);*/
 		KomentarDbModel komModel=new KomentarDbModel();
 		KomentarRepository komRep=new KomentarRepository();
-		DokumentVerzijaDbModel dokVerzija =new DokumentVerzijaDbModel();
+		/*DokumentVerzijaDbModel dokVerzija =new DokumentVerzijaDbModel();
 		DokumentDbModel dok=new DokumentDbModel();
 		FolderDbModel fm =new FolderDbModel();
 		fm.setAktivan(true);
@@ -61,14 +61,14 @@ public class KomentarRepositoryTest {
 		dokVerzija.setDokumentId((int) dokId);
 		dokVerzija.setPostavioKorisnik(korModel);
 		dokVerzija.setPostavioKorisnikId((int) idKor);
-		long dokV=DbDMSContext.getInstance().getDokumentiVerzije().sacuvaj(dokVerzija);
+		long dokV=DbDMSContext.getInstance().getDokumentiVerzije().sacuvaj(dokVerzija);*/
 		komModel.setAktivan(true);
 		komModel.setDatumVrijemeKomentara(new Date());
-		komModel.setDokumentVerzija(dokVerzija);
-		komModel.setDokumentVerzijaId((int) dokV);
+		//komModel.setDokumentVerzija(dokVerzija);
+		//komModel.setDokumentVerzijaId((int) dokV);
 		komModel.setKomentar("neki komentar");
-		komModel.setKorisnik(korModel);
-		komModel.setKorisnikId((int) idKor);
+		//komModel.setKorisnik(korModel);
+		//komModel.setKorisnikId((int) idKor);
 		
 		Assert.assertTrue(komRep.dodajKomentar(komModel));
 		
@@ -93,6 +93,59 @@ public class KomentarRepositoryTest {
 
 	@Test
 	public void testDajKomentare() {
+		/*	KorisnikDbModel korModel = new KorisnikDbModel();
+		KorisnikPozicijaDbModel pozicija = new KorisnikPozicijaDbModel();
+		KorisnikTipDbModel korTip = new KorisnikTipDbModel();
+		korTip.setAktivan(true);
+		korTip.setKorisnikTipNaziv("Administrator");
+		long idTipa =DbDMSContext.getInstance().getKorisnikTipovi().sacuvaj(korTip);
+		pozicija.setKorisnikPozicijaNaziv("neki direktor");
+		pozicija.setAktivan(true);
+		long idPozicije = DbDMSContext.getInstance().getKorisnikPozicije().sacuvaj(pozicija);
+		korModel.setAdresa("neka adresa");
+		korModel.setAktivan(true);
+		korModel.setDatumRodjenja(new Date());
+		korModel.setIme("neko ime");
+		korModel.setKorisnikPozicija(pozicija);
+		korModel.setKorisnikPozicijaId((int) idPozicije);
+		korModel.setKorisnikTip(korTip);
+		korModel.setKorisnikTipId((int) idTipa);
+		korModel.setPassword("pass");
+		korModel.setPrezime("neko prezime");
+		korModel.setUsername("user");
+		long idKor = DbDMSContext.getInstance().getKorisnici().sacuvaj(korModel);*/
+		KomentarDbModel komModel=new KomentarDbModel();
+		KomentarRepository komRep=new KomentarRepository();
+		DokumentVerzijaDbModel dokVerzija =new DokumentVerzijaDbModel();
+		/*DokumentDbModel dok=new DokumentDbModel();
+		FolderDbModel fm =new FolderDbModel();
+		fm.setAktivan(true);
+		fm.setFolderNaziv("naziv foldera");
+		fm.setKreiraoKorisnikId((int) korModel.getKorisnikID());
+		long folderId=DbDMSContext.getInstance().getFolderi().sacuvaj(fm);
+		dok.setDokumentNaziv("neki naziv dokumenta");
+		dok.setEkstenzija("exe");
+		dok.setFolder(fm);
+		dok.setFolderId((int) folderId);
+		long dokId=DbDMSContext.getInstance().getDokumenti().sacuvaj(dok);*/
+		dokVerzija.setAktivan(true);
+		//dokVerzija.setDokument(dok);
+		//dokVerzija.setDokumentId((int) dokId);
+		//dokVerzija.setPostavioKorisnik(korModel);
+		//dokVerzija.setPostavioKorisnikId((int) idKor);
+		long dokV=DbDMSContext.getInstance().getDokumentiVerzije().sacuvaj(dokVerzija);
+		komModel.setAktivan(true);
+		komModel.setDatumVrijemeKomentara(new Date());
+		komModel.setDokumentVerzija(dokVerzija);
+		komModel.setDokumentVerzijaId((int) dokV);
+		komModel.setKomentar("neki komentar");
+		long komId= DbDMSContext.getInstance().getKomentari().sacuvaj(komModel);
+		//komModel.setKorisnik(korModel);
+		//komModel.setKorisnikId((int) idKor);
+		int brKom=komRep.dajKomentare((int) dokV).size();
+		komRep.dodajKomentar(komModel);
+		Assert.assertEquals(brKom+1, komRep.dajKomentare((int) dokV).size());
+		
 		//fail("Not yet implemented");
 	}
 
